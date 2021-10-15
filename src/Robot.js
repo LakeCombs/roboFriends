49500ce9369df71800714a96a549cfd37e5a2135
LakeCombs <adesolaolamilekan2@gmail.com>
2021-10-15 10:08:42 +0100

    robo friends

---
+++ src/Robot.js
@@ -0,0 +1,19 @@
+export const robots = [
+  { id: 1, name: "lakes wheels", username: "lakes", email: "lake@gmail.com" },
+  {
+    id: 2,
+    name: "combs crimbly",
+    username: "conbsii",
+    email: "combs@gmail.com",
+  },
+  { id: 3, name: "crown gray", username: "lakes", email: "lake@gmail.com" },
+
+  { id: 4, name: "types lee", username: "types", email: "type@gmail.com" },
+  { id: 5, name: "reel grit", username: "rigt", email: "realgrit@gmail.com" },
+  {
+    id: 6,
+    name: "shiman anki",
+    username: "anki lw",
+    email: "ankikiku@gmail.com",
+  },
+];
---
+++ src/components/Scroll.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Scroll = ({ children }) => {
+  console.log(children);
+  return (
+    <div
+      style={{
+        overflow: "scroll",
+        border: "5px solid black",
+        height: "800px",
+      }}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Scroll;
